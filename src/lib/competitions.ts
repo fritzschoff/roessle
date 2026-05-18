f001e57d2900711225b42b3f732b8ebfa4d98302
fritzschoff <fritzschoff@icloud.com>
2026-05-18 13:54:54 +0100

    feat: add static teams and competitions config

---
+++ src/lib/competitions.ts
@@ -0,0 +1,13 @@
+export type Competition = {
+  name: string;
+  logo: string;
+};
+
+export const COMPETITIONS: Competition[] = [
+  { name: "Bundesliga", logo: "/images/competitions/bundesliga.svg" },
+  { name: "DFB-Pokal", logo: "/images/competitions/dfb-pokal.svg" },
+  { name: "Champions League", logo: "/images/competitions/champions-league.svg" },
+  { name: "Europa League", logo: "/images/competitions/europa-league.svg" },
+  { name: "Conference League", logo: "/images/competitions/conference-league.svg" },
+  { name: "Freundschaftsspiel", logo: "/images/competitions/freundschaftsspiel.svg" },
+];
---
+++ src/lib/teams.ts
@@ -0,0 +1,41 @@
+export type Team = {
+  name: string;
+  logo: string;
+};
+
+export const TEAMS: Team[] = [
+  { name: "Noch unbekannt", logo: "/images/teams/placeholder.svg" },
+  // Bundesliga
+  { name: "FC Bayern München", logo: "/images/teams/fcb.svg" },
+  { name: "Borussia Dortmund", logo: "/images/teams/bvb.svg" },
+  { name: "Bayer 04 Leverkusen", logo: "/images/teams/b04.svg" },
+  { name: "RB Leipzig", logo: "/images/teams/rbl.svg" },
+  { name: "Eintracht Frankfurt", logo: "/images/teams/sge.svg" },
+  { name: "SC Freiburg", logo: "/images/teams/scf.svg" },
+  { name: "Borussia Mönchengladbach", logo: "/images/teams/bmg.svg" },
+  { name: "TSG 1899 Hoffenheim", logo: "/images/teams/tsg.svg" },
+  { name: "1. FC Union Berlin", logo: "/images/teams/fcu.svg" },
+  { name: "1. FSV Mainz 05", logo: "/images/teams/m05.svg" },
+  { name: "SV Werder Bremen", logo: "/images/teams/svw.svg" },
+  { name: "FC Augsburg", logo: "/images/teams/fca.svg" },
+  { name: "VfL Wolfsburg", logo: "/images/teams/wob.svg" },
+  { name: "1. FC Heidenheim 1846", logo: "/images/teams/fch.svg" },
+  { name: "Holstein Kiel", logo: "/images/teams/ksh.svg" },
+  { name: "FC St. Pauli", logo: "/images/teams/fcsp.svg" },
+  { name: "VfL Bochum", logo: "/images/teams/vflb.svg" },
+  // DFB Pokal regulars
+  { name: "Hamburger SV", logo: "/images/teams/hsv.svg" },
+  { name: "FC Schalke 04", logo: "/images/teams/s04.svg" },
+  { name: "Hertha BSC", logo: "/images/teams/bsc.svg" },
+  { name: "1. FC Nürnberg", logo: "/images/teams/fcn.svg" },
+  { name: "Hannover 96", logo: "/images/teams/h96.svg" },
+  { name: "Fortuna Düsseldorf", logo: "/images/teams/f95.svg" },
+  { name: "Karlsruher SC", logo: "/images/teams/ksc.svg" },
+  { name: "1. FC Kaiserslautern", logo: "/images/teams/fck.svg" },
+  { name: "SpVgg Greuther Fürth", logo: "/images/teams/sgf.svg" },
+];
+
+export const VFB: Team = {
+  name: "VfB Stuttgart",
+  logo: "/images/teams/vfb.svg",
+};
